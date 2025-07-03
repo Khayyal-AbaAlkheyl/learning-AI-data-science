15b439460c3f8529f7be578bbb44a712849d559d
Khayyal-AbaAlkheyl <khayyalabd@gmail.com>
2025-07-03 09:08:44 +0300

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,17 @@
+lion01="********************"#header / fopter 
+
+lion02="*                  *"
+lion03="*     Welocome!    *"
+        lion4="******* "
+
+print('')
+print(lion01)
+print(lion02)
+print(lion03)
+print(lion02)
+print(lion01)
+
+
+
+
+
